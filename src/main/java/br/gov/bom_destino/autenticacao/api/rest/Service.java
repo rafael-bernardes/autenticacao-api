b9742e8e7a5a32e958d8fe80f877fa6c8a8593e1
Rafael Pereira <rafael.bernardes@live.com>
2021-04-01 09:59:59 -0300

    Renomeia pacote

---
+++ src/main/java/br/gov/bom_destino/autenticacao/api/rest/Service.java
@@ -0,0 +1,9 @@
+package br.gov.bom_destino.autenticacao.api.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class Service extends Application {
+
+}
